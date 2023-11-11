35a053c393f64ad388692db501dd22eb522d09a8
Nhatminh309 <nhatminh2k22k3@gmail.com>
2023-11-11 17:00:02 +0700

    merge topic/method-overloading to main

--- Lab02/AimsProject/src/Cart.java
+++ Lab02/AimsProject/src/Cart.java
@@ -51,4 +51,55 @@ public class Cart {
         }
         return total; // Trả về tổng giá trị.
     }
+
+// Lab03
+
+    // 2.1 create a new method that has the same name but with different type of parameter.
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList){
+        // Biến lưu trữ vị trí thêm hiện tại của mảng
+        int loopList = 0;
+        // Kiểm tra xem giỏ hàng còn chỗ trống không.
+        if (qtyOrdered < MAX_NUMBERS_ORDERS) {
+            // Thêm đĩa DVD vào vị trí tiếp theo trong mảng.
+            itemsOrdered[qtyOrdered] = dvdList[loopList];
+            // Tăng số lượng đơn đặt hàng.
+            qtyOrdered++;
+            // Vị trí trong mảng tiếp theo được cập nhật
+            loopList++;
+            System.out.println("Added."); // In ra thông báo đã thêm đơn đặt hàng thành công.
+        } else {
+            System.out.println("The cart is full"); // In ra thông báo nếu giỏ hàng đã đầy.
+        }
+    }
+
+    // Allows to pass an arbitrary number of arguments for dvd
+    public void addDigitalVideoDisc(String dvdTitle){
+        // Kiểm tra xem giỏ hàng còn chỗ trống không.
+        if (qtyOrdered < MAX_NUMBERS_ORDERS) {
+            // Thêm đĩa DVD vào vị trí tiếp theo trong mảng.
+            itemsOrdered[qtyOrdered] = new DigitalVideoDisc(dvdTitle);
+            // Tăng số lượng đơn đặt hàng.
+            qtyOrdered++;
+            System.out.println("Added."); // In ra thông báo đã thêm đơn đặt hàng thành công.
+        } else {
+            System.out.println("The cart is full"); // In ra thông báo nếu giỏ hàng đã đầy.
+        }
+    }
+
+    // 2.2 Overloading by differing the number of parameters
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2){
+        // Kiểm tra xem giỏ hàng còn chỗ trống không.
+        if (qtyOrdered < MAX_NUMBERS_ORDERS - 1) {
+            // Thêm đĩa DVD vào vị trí tiếp theo trong mảng.
+            itemsOrdered[qtyOrdered] = dvd1;
+            itemsOrdered[qtyOrdered] = dvd2;
+            // Tăng số lượng đơn đặt hàng.
+            qtyOrdered += 2;
+            System.out.println("Added."); // In ra thông báo đã thêm đơn đặt hàng thành công.
+        } else {
+            System.out.println("The cart is full"); // In ra thông báo nếu giỏ hàng đã đầy.
+        }
+    }
 }
+
+
